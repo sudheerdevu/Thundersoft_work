89943b0e9b467ad3bb0354d2167ce8d42e8427cc
sudheerdevu <sudheerdevu.vs3@gmail.com>
2024-10-03 11:44:09 +0530

    11:44

--- cpp_concepts/pointer_to_a_object.cpp
+++ cpp_concepts/pointer_to_a_object.cpp
@@ -13,11 +13,18 @@ int perimeter(){
 };
 
 int main(){
-Rectangle r1; // using pointers on objects i.e., r1 object of class Rectangle
-Rectangle *ptr=&r1;
+    
+// Rectangle r1; // using pointers on objects i.e., r1 object of class Rectangle
+// Rectangle *ptr=&r1;
+//instead of crating an object directly lets try to create in heap
+Rectangle *ptr=new Rectangle();
 ptr->length=10;
 ptr->breadth=5;
 std::cout<<ptr->area()<<std::endl;
 std::cout<<ptr->perimeter()<<std::endl;
+
+//same result
+//50
+//30
 
 }
