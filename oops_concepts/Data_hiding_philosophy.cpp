c1c3060899513fad51e782de751b0ce8e72ecb5a
sudheerdevu <sudheerdevu.vs3@gmail.com>
2024-10-03 16:11:57 +0530

    4:11pm

--- oops_concepts/Data_hiding_philosophy.cpp
+++ oops_concepts/Data_hiding_philosophy.cpp
@@ -1,3 +1,7 @@
+/*
+
 According to oops principles the data of the class variables must be hidden
 so the data members are made private and functions are made as public 
 these private data members can be accessed through functions....
+
+*/
