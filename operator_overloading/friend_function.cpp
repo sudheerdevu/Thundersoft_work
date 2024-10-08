b1da45b2fa2f4887a1b3bba8f28c5e6fb58b9043
sudheerdevu <sudheerdevu.vs3@gmail.com>
2024-10-08 15:29:37 +0530

    3:29pm

--- operator_overloading/friend_function.cpp
+++ operator_overloading/friend_function.cpp
@@ -1,26 +1,29 @@
-#include<iostream>
-class Complex{
-    private:
-    int real;int img;
-    public:
-    Complex(int r=0,int i=0){
-        real=r;img=i;
+#include <iostream>
+class Complex {
+private:
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0) : real(r), img(i) {}
+
+    void display() {
+        std::cout << real << "+i" << img << std::endl;
     }
-    void display(){
-        std::cout<<real<<"+i"<<img<<std::endl;
-    }
-    Complex operator+(Complex c){
-        Complex temp;
-        temp.real=real+c.real;
-        temp.img=img+c.img;
-        return temp;
-    }
+
+    friend Complex operator+(Complex c1, Complex c2);
 };
-int main(){
-    Complex c1,c2,c3;
-    c1.real=5;c1.img=3;
-    c2.real=3;c2.img=2;
-    //c3=c1.add(c2);
-    c3=c1+c2;
-    std::cout<<c3.real<<"+i"<<c3.img<<std::endl;
+
+Complex operator+(Complex c1, Complex c2) {
+    Complex temp;
+    temp.real = c1.real + c2.real;
+    temp.img = c1.img + c2.img;
+    return temp;
+}
+
+int main() {
+    Complex c1(5, 3), c2(2, 3), c3;
+    c3 = c1 + c2;
+    c3.display();
+    return 0;
 }
