56d4aa770f8e73c44ddfd5c7024ad8e71865f8a9
sudheerdevu <sudheerdevu.vs3@gmail.com>
2024-10-27 21:50:13 +0530

    9:50 pm

--- Personal/RUN/qwerty.cpp
+++ Personal/RUN/qwerty.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
-int main() {
-    int x = 10;         // Regular integer
-    int* ptr = &x;      // Pointer to integer, initialized to the address of x
-
-    // Output the address and value
-    std::cout << "Address of x: " << ptr << std::endl;
-    std::cout << "Value of x via pointer: " << *ptr << std::endl;
-
-    // Modify the value through the pointer
-    *ptr = 20;
-    std::cout << "New value of x: " << x << std::endl;
+int main()
+{
+    int x { 5 };
+    ++x; // This expression statement has the side-effect of incrementing x
+    std::cout << x << '\n'; // prints 6
 
     return 0;
 }
-
